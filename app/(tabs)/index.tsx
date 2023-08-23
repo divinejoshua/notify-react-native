712be2806a0438cea25a1ceea5ae42bee43d77f6
divinejoshua <divineuche399@gmail.com>
2023-08-23 17:36:39 +0100

    Added notification configurations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,8 @@ export default function TabOneScreen() {
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+      
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,22 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Button, Platform, useColorScheme } from 'react-native';
+
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { Text, View } from '../components/Themed';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -19,6 +33,15 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+  // Notification data 
+  const [expoPushToken, setExpoPushToken] = useState<any>('');
+  const [notification, setNotification] = useState<any>(false);
+  const notificationListener = useRef<any>();
+  const responseListener = useRef<any>();
+
+
+
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
@@ -35,11 +58,57 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
+
+    //Use effect for notification
+    useEffect(() => {
+      registerForPushNotificationsAsync().then((token : any) => setExpoPushToken(token));
+
+      notificationListener.current = Notifications.addNotificationReceivedListener((notification : any) => {
+        setNotification(notification);
+      });
+
+      responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response);
+      });
+
+      return () => {
+        Notifications.removeNotificationSubscription(notificationListener.current);
+        Notifications.removeNotificationSubscription(responseListener.current);
+      };
+    }, []);
+
+
+
   if (!loaded) {
     return null;
   }
 
-  return <RootLayoutNav />;
+
+  return (
+
+    <View
+    style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    }}>
+    <Text>Your expo push token: {expoPushToken}</Text>
+    <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Title: {notification && notification.request.content.title} </Text>
+      <Text>Body: {notification && notification.request.content.body}</Text>
+      <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+    </View>
+    <Button
+      title="Press to schedule a notification"
+      onPress={async () => {
+        await schedulePushNotification();
+      }}
+    />
+  </View>
+
+
+  )
+  // return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
@@ -52,5 +121,51 @@ function RootLayoutNav() {
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
+
+   
   );
+}
+
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
 }
